31b6452d38b5b7e3f5acb9bd799942e217472ebb
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-06-09 18:48:27 -0400

    Removed duplicate port IDs

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var db = require("./models");
 // Create an instance of the express app.
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
